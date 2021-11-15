0cc24fb6f6cab4cca41ce1e1e31a2ac967338056
alvestad10 <alvestad10@gmail.com>
2021-11-15 19:10:45 +0100

    Adding pretty print to solvers

--- src/Solver.jl
+++ src/Solver.jl
@@ -8,7 +8,7 @@ Geometric Euler-Maruyama scheme
 Based in the N=1 approximation of the Magnus solution
 """
 struct gEM <: Solver end
-
+Base.show(io::IO, alg::gEM) = print(io, "gEM")
 
 """
 Implicit Geometric Euler-Maruyama scheme
@@ -19,6 +19,7 @@ where θ refferes to the impliciteness of the scheme
 struct gθEM <: Solver
     θ::Float64
 end
+Base.show(io::IO, alg::gθEM) = print(io, "gθEM: θ=", alg.θ)
 
 """
 Geometric Heuns scheme
@@ -26,6 +27,7 @@ Geometric Heuns scheme
 Based in the N=2 approximation of the Magnus solution
 """
 struct gHeuns <: Solver end
+Base.show(io::IO, alg::gHeuns) = print(io, "gHeuns")
 
 """
 Geometric Implicit Midpoint scheme
@@ -33,7 +35,7 @@ Geometric Implicit Midpoint scheme
 Based in the N=2 approximation of the Magnus solution
 """
 struct gMidpoint <: Solver end
-
+Base.show(io::IO, alg::gMidpoint) = print(io, "gMidpoint")
 
 """
 RK
